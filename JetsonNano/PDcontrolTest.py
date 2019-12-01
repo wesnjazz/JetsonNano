e0215e24c5e3794a9b83df982710489adbf721a6
wesnjazz <dongwonpark@umass.edu>
2019-12-01 03:10:29 -0500

    K:2.0 B:2.0 no Threshold works well.

--- JetsonNano/PDcontrolTest.py
+++ JetsonNano/PDcontrolTest.py
@@ -28,12 +28,12 @@ def read_serial():
     if len(line) >= 2:
         # print(line)
         for i in range(0, len(line)): line[i] = int(line[i])
-        print("L:{} R:{}".format(line[1], line[0]))
+        # print("L:{} R:{}".format(line[1], line[0]))
         lastLtick = line[1]
         lastRtick = line[0]
         return line[1], line[0]
     else:
-        print("\tLast L:{} R:{}".format(lastLtick, lastRtick))
+        # print("\tLast L:{} R:{}".format(lastLtick, lastRtick))
         return lastLtick, lastRtick
 
 def reset_ticks():
@@ -43,7 +43,7 @@ def reset_ticks():
 def getAngle(l_ticks, r_ticks, one_cm_per_tick=0.0107, wheel_base=11.3):
     delta_l = one_cm_per_tick * l_ticks
     delta_r = one_cm_per_tick * r_ticks
-    delta_s = (delta_r - delta_l) / 2.0
+    delta_s = (delta_l - delta_r) / 2.0
     delta_theta = delta_s / (wheel_base / 2.0)
     delta_theta_in_radian = delta_s / (wheel_base / 2.0)
     delta_theta_in_degree = (180.0 / math.pi) * delta_theta
@@ -69,12 +69,9 @@ def getActPositionXY_atTimeT(current_x, delta_theta):
 # def speedToPWM(speed):
 
 
-def drive(ser, robot, r_speed, l_speed, distance):
+def drive(ser, robot, l_speed, r_speed, distance):
     # number of ticks 't' to travel 'x' distance:
     # t = x / 0.0106 (in cm)
-
-    # Drive the robot
-    robot.set_motors(r_speed, l_speed)
 
     # Calculate required ticks to travel given distance
     ticks = int(distance / 0.0107)
@@ -96,6 +93,7 @@ def drive(ser, robot, r_speed, l_speed, distance):
 
     one_cm_per_tick = 0.0106
     wheel_base = 11.3
+    Yoke = 4 # distance from the center of wheelbase in cm
     start_time = time.time()
     l_tick_last = 0
     r_tick_last = 0
@@ -103,6 +101,17 @@ def drive(ser, robot, r_speed, l_speed, distance):
     accumulatedY = 0
     accumulatedTheta = 0
     last_tick = 0
+    theta_ddot_last = 0
+    l_speed_last = 0
+    r_speed_last = 0
+    K = 2.0
+    B = 2.0
+    # K = l_speed * 10
+    # B = 5
+
+    # Drive the robot
+    robot.set_motors(l_speed, r_speed)
+
     while True:
         l_tick, r_tick = read_serial()
         delta_l_tick = l_tick_last - l_tick
@@ -113,20 +122,53 @@ def drive(ser, robot, r_speed, l_speed, distance):
         accumulatedX += deltaX
         accumulatedY += deltaY
         accumulatedTheta += deltaTheta
+        current_theta = getAngle(l_tick, r_tick)
         delta_t = time.time() - start_time
+
+
+        theta_dot_act = deltaTheta / delta_t
+        theta_ddot = (-K * (current_theta - 0)) - (B * theta_dot_act)
         # xRef, yRef = getRefPositionXY_atTimeT(delta_t, )
 
-        current_theta = getAngle(l_tick, r_tick)
         # xAct, yAct = getActPositionXY_atTimeT(delta_theta)
 
-        print("Goal:({:.3f},{:.3f},{:.3f}) \
-        Act:({:.3f},{:.3f},{:.3f})  RealTheta:{:.3f}".format\
-                  (50, 0, 0, accumulatedX, accumulatedY, accumulatedTheta, current_theta))
+        delta_PWM = theta_ddot / 200.0
+        # l_speed = l_speed_last + (delta_PWM)
+        # r_speed = r_speed_last - (delta_PWM)
+
+        new_l_speed = l_speed + delta_PWM
+        new_r_speed = r_speed - delta_PWM
+        if (new_l_speed < 0):
+            new_l_speed = l_speed
+        if (new_r_speed < 0):
+            new_r_speed = r_speed
+
+        '''
+        left&right speed = 0.3; K=3; B=3; threshold = (K * 9) - (K * B)  
+        '''
+
+        threshold = 500
+        # threshold = (K * 10) * 0.7
+        # threshold = (K * 9) - (K * B)
+        # if abs(theta_ddot - theta_ddot_last) > threshold:
+        #     new_l_speed = l_speed
+        #     new_r_speed = r_speed
+        if abs(theta_ddot) > threshold:
+            new_l_speed = l_speed
+            new_r_speed = r_speed
+        robot.set_motors(new_l_speed, new_r_speed)
+
+        # l_speed_last = l_speed
+        # r_speed_last = r_speed
+        print("""Goal:({:.3f},{:.3f},{:.3f}) Act:({:.3f},{:.3f},{:.3f}) RealTheta:{:.3f} th_dot_act:{:.3f} th_ddot:{:.3f} delta_PWM:{:.3f} speed:({:.3f},{:.3f})""".format\
+                  (50, 0, 0, accumulatedX, accumulatedY, accumulatedTheta, current_theta,\
+                   theta_dot_act, theta_ddot, delta_PWM, l_speed, r_speed))
 
         # PWM_L, PWM_R = PWM_manager()
         # robot.set_motors(PWM_L, PWM_R)
         l_tick_last = l_tick
         r_tick_last = r_tick
+        theta_ddot_last = theta_ddot
         last_tick = (r_tick_last + l_tick_last) // 2
         if l_tick >= ticks:
             print(" Left wheel reached {}. Goal was {}".format(l_tick, ticks))
@@ -158,11 +200,11 @@ def main():
     # args = parser.parse_args()
 
     print("import finished.")
-    # sleep(2)
+    sleep(2)
     robot = Robot()
-    l_speed = 0.3
-    r_speed = 0.3
-    distance = 50
+    l_speed = 0.55
+    r_speed = 0.55
+    distance = 70
     t1 = threading.Thread(target=drive, \
                           args=(s1, robot, l_speed, r_speed, distance))
     t1.start()
